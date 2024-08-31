1bacf507d92419666e8103dfa3f8295bfed7d274
bougzy <yubeespecial@gmail.com>
2024-08-31 16:51:53 +0100

    up

--- index.js
+++ index.js
@@ -501,5 +501,5 @@ app.post('/api/messages', async (req, res) => {
   
 
 app.listen(5000, () => {
-    console.log('Server is running on port 8000');
+    console.log('Server is running on port 5000');
 });
